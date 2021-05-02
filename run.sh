d63470994a3dc0cc3466855143ddc032c9f79181
John de la Garza <jdelagarza@cylance.com>
2021-05-01 19:37:13 -0700

    run: add sound

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 duration=$1
 lift=$2
+
 if [ $# -ne 2 ]; then
   echo "usage: $0 <duration> <lift name>"
   exit 1
 fi
-for n in `seq 3 1`; do
-  echo $n
-  sleep 1
-done
-echo begin
-./read_sound.sh $duration | ./count.py $lift | tee -a db.txt
+
+sleep 2
+play -n  synth 1 sin 300 2>/dev/null
+./read_sound.sh $duration | ./count.py $lift | tee -a $db && \
 play -n  synth 1 sin 300 2>/dev/null
